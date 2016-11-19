200df4bee5580435b6249e774236cf199a5f2b50
Nick Crescimanno <nickcres13@gmail.com>
2016-11-19 15:38:30 -0500

    now we are using servos..

--- pwmTest.py
+++ pwmTest.py
@@ -5,4 +5,5 @@ import pigpio
 
 pi = pigpio.pi() # connect to local Pi
 pi.set_mode(17, pigpio.OUTPUT) # GPIO 17 as output
-pi.set_servo_pulsewidth(17, 1500) # centre
+#pi.set_servo_pulsewidth(17, 1500) # centre
+pi.set_servo_pulsewidth(17, 0)    # off
