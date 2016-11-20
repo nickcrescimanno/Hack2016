173592f1fee8583db628dcef368bdcaf83f2392f
Larry King <larry@digibudgie.com>
2016-11-20 08:13:50 -0500

    Fixed indexing and sign of throttle

--- readin.py
+++ readin.py
@@ -35,8 +35,8 @@ class ControllerInput:
         # input=' '.join(format(ord(x), 'b') for x in message)
         # a=input.split(' ')
         # print '1',ord(message[0]),'2',ord(message[1]),'3',ord(message[2]),'4',ord(message[3])
-        normalized1 = (ord(message[0]) - 128) / 128
-        normalized2 = (ord(message[1]) - 128) / 128
-        normalized3 = (ord(message[1]) - 256) / 256
+        normalized1 = (ord(message[0]) - 128) / 128.0
+        normalized2 = (ord(message[1]) - 128) / 128.0
+        normalized3 = (ord(message[2])) / 256.0
         return normalized1, normalized2, normalized3, ord(message[3])
 
