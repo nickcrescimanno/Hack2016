3f3609e6f01da728cea121a142ff444001f1320f
Larry King <larry@digibudgie.com>
2016-11-20 08:58:45 -0500

    Try to fix delay

--- readin.py
+++ readin.py
@@ -12,13 +12,13 @@ class ControllerInput:
         self.s.listen(1)
         print 'Listening'
 
+        connection, address = self.s.accept()
+
     def close(self):
+        connection.close()
         self.s.close()
 
     def poll(self):
-        # returns latest input from controller
-        connection, address = self.s.accept()
-
         try:
             message = connection.recv(16)
             print "Got Mesage from %s: %s:" % (address, message)
@@ -26,15 +26,7 @@ class ControllerInput:
             raise
         except:
             traceback.print_exc()
-        finally:
-            connection.close()
-        # if len(message) == 4:
-        #	print struct.unpack('bbbb', message)
 
-        # print '1',message[0],'2',ord(message[1]),'3',ord(message[2]),'4',ord(message[3])
-        # input=' '.join(format(ord(x), 'b') for x in message)
-        # a=input.split(' ')
-        # print '1',ord(message[0]),'2',ord(message[1]),'3',ord(message[2]),'4',ord(message[3])
         normalized1 = (ord(message[0]) - 128) / 128.0
         normalized2 = (ord(message[1]) - 128) / 128.0
         normalized3 = (ord(message[2])) / 256.0
